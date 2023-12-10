fd3b25fde5d9da979f72d25d83af10d781283896
myselfatul <atulverma.verma4@gmail.com>
2023-12-10 19:47:47 +0530

    chore: added port

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn'],
   });
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000, "0.0.0.0");
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
